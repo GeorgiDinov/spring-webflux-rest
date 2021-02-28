f5b623cfcbddc3c7140a9b543000844feb63e7d0
Georgi Dinov <georgidinov1@abv.bg>
2021-02-28 15:10:41 +0200

    Added category controller tests.

---
+++ src/test/java/com/georgidinov/springwebfluxrest/controller/CategoryControllerTest.java
@@ -0,0 +1,57 @@
+package com.georgidinov.springwebfluxrest.controller;
+
+import com.georgidinov.springwebfluxrest.domain.Category;
+import com.georgidinov.springwebfluxrest.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.anyString;
+
+class CategoryControllerTest {
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    WebTestClient webTestClient;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    void findAllCategories() {
+        BDDMockito.given(categoryRepository.findAll()).willReturn(Flux.just(
+                Category.builder().description("Fruits").build(),
+                Category.builder().description("Nuts").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+
+    }
+
+    @Test
+    void findCategoryById() {
+        BDDMockito.given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().description("Fruits").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/someId")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
