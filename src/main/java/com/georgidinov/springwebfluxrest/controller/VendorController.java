b55017c90e892d4ecde54981c2d5d258b4f7ebf7
Georgi Dinov <georgidinov1@abv.bg>
2021-02-28 20:54:26 +0200

    Added vendor post and put methods and tests.

--- src/main/java/com/georgidinov/springwebfluxrest/controller/VendorController.java
+++ src/main/java/com/georgidinov/springwebfluxrest/controller/VendorController.java
@@ -4,10 +4,14 @@ package com.georgidinov.springwebfluxrest.controller;
 import com.georgidinov.springwebfluxrest.domain.Vendor;
 import com.georgidinov.springwebfluxrest.repository.VendorRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -39,5 +43,23 @@ public class VendorController {
         log.info("VendorController::findVendorById");
         return this.vendorRepository.findById(id);
     }
+
+    @PostMapping("/api/v1/vendors")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Void> createVendor(@RequestBody Publisher<Vendor> vendorPublisher) {
+        log.info("VendorController::createVendor -> stream passed = {}", vendorPublisher);
+        return this.vendorRepository.saveAll(vendorPublisher).then();
+    }
+
+
+    @PutMapping("/api/v1/vendors/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Mono<Vendor> updateVendor(@PathVariable String id,
+                                     @RequestBody Vendor vendor) {
+        log.info("VendorController::updateVendor -> id passed  = {}, vendor passed = {}", id, vendor);
+        vendor.setId(id);
+        return this.vendorRepository.save(vendor);
+    }
+
 
 }
--- src/test/java/com/georgidinov/springwebfluxrest/controller/VendorControllerTest.java
+++ src/test/java/com/georgidinov/springwebfluxrest/controller/VendorControllerTest.java
@@ -1,5 +1,6 @@
 package com.georgidinov.springwebfluxrest.controller;
 
+import com.georgidinov.springwebfluxrest.domain.Category;
 import com.georgidinov.springwebfluxrest.domain.Vendor;
 import com.georgidinov.springwebfluxrest.repository.VendorRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,10 +9,12 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 class VendorControllerTest {
@@ -54,5 +57,35 @@ class VendorControllerTest {
                 .exchange()
                 .expectBody(Vendor.class);
 
+    }
+
+    @Test
+    void createVendor() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().name("Vendor Name").build()));
+
+        Mono<Vendor> vendorToSavedMono = Mono.just(Vendor.builder().name("The Vendor").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToSavedMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    void updateVendor() {
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().name("Vendor Name").build()));
+
+        Mono<Vendor> vendorToUpdatedMono = Mono.just(Vendor.builder().name("The Vendor").build());
+
+        webTestClient.put()
+                .uri("/api/v1/vendors/someId")
+                .body(vendorToUpdatedMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
